401d9ace3f973c7f27503eb0b393860c86a75db0
张蕴鹏 <zhangyp_csd@si-tech.com.cn>
2018-11-30 15:10:35 +0800

    商城商户管理平台后端首次提交

---
+++ src/main/java/github/beginner/noname/NonameApplication.java
@@ -0,0 +1,12 @@
+package github.beginner.noname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonameApplication.class, args);
+	}
+}
