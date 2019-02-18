b81d00c76392eabb72ec6f9cc1deb2b13835d85a
zhangyp <461476492@qq.com>
2019-02-18 16:53:17 +0800

    feat: 加入jwt依赖包

--- src/main/java/github/beginner/noname/NonameApplication.java
+++ src/main/java/github/beginner/noname/NonameApplication.java
@@ -2,12 +2,16 @@ package github.beginner.noname;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 
 /**
+ * ServletComponentScan:
+ *  servlet, Filter, Listener可以直接通过@WebServlet @WebFilter @WebListener来进行自动注册
  * @author springboot.io
  */
 @SpringBootApplication
+@ServletComponentScan
 public class NonameApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NonameApplication.class, args);
---
+++ src/main/java/github/beginner/noname/util/JwtUtils.java
@@ -0,0 +1,7 @@
+package github.beginner.noname.util;
+
+/**
+ * @author zyp on 2019/2/18
+ */
+public class JwtUtils {
+}
